e8924791366225c8d213d4b1d149a4a9bb4f1234
Matt Brown <mattbrown@spotify.com>
2017-12-29 11:21:03 -0500

    address checkstyle violations in folsom-semantic-metrics
    yay for having different rule sets in different repos :/

--- folsom-semantic-metrics/src/main/java/com/spotify/folsom/client/SemanticFolsomMetrics.java
+++ folsom-semantic-metrics/src/main/java/com/spotify/folsom/client/SemanticFolsomMetrics.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014-2015 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.spotify.folsom.client;
 
 import com.codahale.metrics.Gauge;
@@ -14,7 +29,8 @@ import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 /**
- * {@link com.spotify.folsom.Metrics} implementation using semantic-metrics. Mostly a port of the YammerMetrics class.
+ * {@link com.spotify.folsom.Metrics} implementation using semantic-metrics.
+ * Mostly a port of the YammerMetrics class.
  */
 public class SemanticFolsomMetrics implements Metrics {
 
@@ -73,7 +89,8 @@ public class SemanticFolsomMetrics implements Metrics {
     // the two meters can be summed to count total number of successes.
     MetricId getMetersId = MetricId.join(getId, meterId);
     this.getHits = registry.meter(getMetersId.tagged("result", "success", "cache-result", "hit"));
-    this.getMisses = registry.meter(getMetersId.tagged("result", "success", "cache-result", "miss"));
+    this.getMisses = registry.meter(
+            getMetersId.tagged("result", "success", "cache-result", "miss"));
     this.getFailures = registry.meter(getMetersId.tagged("result", "failure"));
 
     // ratio of cache hits to total attempts
--- folsom-semantic-metrics/src/test/java/com/spotify/folsom/client/SemanticFolsomMetricsTest.java
+++ folsom-semantic-metrics/src/test/java/com/spotify/folsom/client/SemanticFolsomMetricsTest.java
@@ -1,29 +1,42 @@
+/*
+ * Copyright (c) 2014-2015 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.spotify.folsom.client;
 
+import com.codahale.metrics.Gauge;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Uninterruptibles;
-
-import com.codahale.metrics.Gauge;
 import com.spotify.folsom.GetResult;
 import com.spotify.folsom.MemcacheStatus;
 import com.spotify.folsom.Metrics.OutstandingRequestsGauge;
-import com.spotify.futures.CompletableFutures;
 import com.spotify.metrics.core.MetricId;
 import com.spotify.metrics.core.SemanticMetricRegistry;
-
-import java.util.concurrent.CompletableFuture;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.SortedMap;
 import java.util.concurrent.TimeUnit;
 
+import static com.spotify.futures.CompletableFutures.exceptionallyCompletedFuture;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.junit.Assert.assertEquals;
 
 public class SemanticFolsomMetricsTest {
 
-  private final OutstandingRequestsGauge GAUGE = () -> 123;
+  private static final OutstandingRequestsGauge GAUGE = () -> 123;
 
   private final SemanticMetricRegistry registry = new SemanticMetricRegistry();
   private final SemanticFolsomMetrics metrics =
@@ -31,7 +44,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testGetMiss() {
-    metrics.measureGetFuture(CompletableFuture.completedFuture(null));
+    metrics.measureGetFuture(completedFuture(null));
 
     assertEquals(1, metrics.getGets().getCount());
     assertEquals(1, metrics.getGetMisses().getCount());
@@ -41,7 +54,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testGetHit() {
-    metrics.measureGetFuture(CompletableFuture.completedFuture(GetResult.success(new byte[]{}, 0)));
+    metrics.measureGetFuture(completedFuture(GetResult.success(new byte[]{}, 0)));
 
     assertEquals(1, metrics.getGets().getCount());
     assertEquals(0, metrics.getGetMisses().getCount());
@@ -51,7 +64,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testGetFailure() {
-    metrics.measureGetFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureGetFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getGets().getCount());
     assertEquals(0, metrics.getGetMisses().getCount());
@@ -62,7 +75,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testSetSuccess() {
-    metrics.measureSetFuture(CompletableFuture.completedFuture(MemcacheStatus.OK));
+    metrics.measureSetFuture(completedFuture(MemcacheStatus.OK));
 
     assertEquals(1, metrics.getSets().getCount());
     assertEquals(1, metrics.getSetSuccesses().getCount());
@@ -71,7 +84,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testSetFailure() {
-    metrics.measureSetFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureSetFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getSets().getCount());
     assertEquals(0, metrics.getSetSuccesses().getCount());
@@ -80,7 +93,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testMultigetSuccess() {
-    metrics.measureMultigetFuture(CompletableFuture.completedFuture(
+    metrics.measureMultigetFuture(completedFuture(
         ImmutableList.of(
             GetResult.success(new byte[]{1}, 0),
             GetResult.success(new byte[]{1}, 0)
@@ -96,7 +109,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testMultigetFailure() {
-    metrics.measureMultigetFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureMultigetFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getMultigets().getCount());
     assertEquals(0, metrics.getMultigetSuccesses().getCount());
@@ -105,7 +118,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testDeleteSuccess() {
-    metrics.measureDeleteFuture(CompletableFuture.completedFuture(MemcacheStatus.OK));
+    metrics.measureDeleteFuture(completedFuture(MemcacheStatus.OK));
 
     assertEquals(1, metrics.getDeletes().getCount());
     assertEquals(1, metrics.getDeleteSuccesses().getCount());
@@ -114,7 +127,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testDeleteFailure() {
-    metrics.measureDeleteFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureDeleteFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getDeletes().getCount());
     assertEquals(0, metrics.getDeleteSuccesses().getCount());
@@ -123,7 +136,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testIncrDecrSuccess() {
-    metrics.measureIncrDecrFuture(CompletableFuture.completedFuture(1L));
+    metrics.measureIncrDecrFuture(completedFuture(1L));
 
     assertEquals(1, metrics.getIncrDecrs().getCount());
     assertEquals(1, metrics.getIncrDecrSuccesses().getCount());
@@ -132,7 +145,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testIncrDecrFailure() {
-    metrics.measureIncrDecrFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureIncrDecrFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getIncrDecrs().getCount());
     assertEquals(0, metrics.getIncrDecrSuccesses().getCount());
@@ -141,7 +154,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testTouchSuccess() {
-    metrics.measureTouchFuture(CompletableFuture.completedFuture(MemcacheStatus.OK));
+    metrics.measureTouchFuture(completedFuture(MemcacheStatus.OK));
 
     assertEquals(1, metrics.getTouches().getCount());
     assertEquals(1, metrics.getTouchSuccesses().getCount());
@@ -150,7 +163,7 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testTouchFailure() {
-    metrics.measureTouchFuture(CompletableFutures.exceptionallyCompletedFuture(new RuntimeException()));
+    metrics.measureTouchFuture(exceptionallyCompletedFuture(new RuntimeException()));
 
     assertEquals(1, metrics.getTouches().getCount());
     assertEquals(0, metrics.getTouchSuccesses().getCount());
@@ -175,16 +188,16 @@ public class SemanticFolsomMetricsTest {
 
   @Test
   public void testGetAndMultigetHitRatio() {
-    metrics.measureGetFuture(CompletableFuture.completedFuture(GetResult.success(new byte[]{1}, 1)));
-    metrics.measureGetFuture(CompletableFuture.completedFuture(missResult()));
-    metrics.measureGetFuture(CompletableFuture.completedFuture(GetResult.success(new byte[]{2}, 1)));
+    metrics.measureGetFuture(completedFuture(GetResult.success(new byte[]{1}, 1)));
+    metrics.measureGetFuture(completedFuture(missResult()));
+    metrics.measureGetFuture(completedFuture(GetResult.success(new byte[]{2}, 1)));
 
     final List<GetResult<byte[]>> list = Lists.newArrayList(
         GetResult.success(new byte[]{3}, 3),
         GetResult.success(new byte[]{4}, 4),
         missResult()
     );
-    metrics.measureMultigetFuture(CompletableFuture.completedFuture(list));
+    metrics.measureMultigetFuture(completedFuture(list));
 
     assertEquals(4, metrics.getGetHits().getCount());
     assertEquals(2, metrics.getGetMisses().getCount());
