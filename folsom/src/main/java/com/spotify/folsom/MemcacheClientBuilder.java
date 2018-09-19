21ac89dfc3f9d75ff3889cf9e6bd11659a9abe65
Daniel Norberg <dano@spotify.com>
2018-09-19 11:59:18 +0900

    make checkstyle happy

--- folsom/src/main/java/com/spotify/folsom/MemcacheClientBuilder.java
+++ folsom/src/main/java/com/spotify/folsom/MemcacheClientBuilder.java
@@ -46,7 +46,6 @@ import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
@@ -308,9 +307,9 @@ public class MemcacheClientBuilder<V> {
    *
    * If null is specified, replies will be executed on EventLoopGroup directly.
    *
-   * <b>Note:</b> Calling non-async methods on the {@link CompletionStage}s returned by
-   * {@link MemcacheClient} that have already completed will cause the supplied function
-   * to be executed directly on the calling thread.
+   * <b>Note:</b> Calling non-async methods on the {@link java.util.concurrent.CompletionStage}s
+   * returned by {@link MemcacheClient} that have already completed will cause the supplied
+   * function to be executed directly on the calling thread.
    *
    * @param executor the executor to use.
    * @return itself
