6c359fceae44e0dec9fa22d5a0792361920842da
Niklas Gustavsson <niklas@protocol7.com>
2018-11-09 14:35:19 +0100

    Fix Mockito deprecations

--- folsom/src/test/java/com/spotify/folsom/RecoveryTest.java
+++ folsom/src/test/java/com/spotify/folsom/RecoveryTest.java
@@ -21,7 +21,7 @@ import static org.hamcrest.Matchers.is;
 import static org.jboss.netty.buffer.ChannelBuffers.copiedBuffer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
@@ -42,7 +42,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
